b694f68655c9f775f6590d89c49c86446f201f81
murata0531 <coless115@gmail.com>
2020-10-03 21:23:54 +0900

    a

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Route::get('/', function () {
+//    return view('welcome');
+// })->middleware('auth');
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
