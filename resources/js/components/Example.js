d04c7e480e1aef462263dd1aeaf9c7546283df2a
murata0531 <coless115@gmail.com>
2020-11-23 19:34:51 +0900

    a

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -6,7 +6,52 @@ export default class Example extends React.Component {
         return (
 
             <div>
-                <input type="text" className="search" placeholder="&#xf002; 検索" ></input>
+                <div className="side">
+                    {/*ドロワーメニュー*/}
+                    <div className="drawer-menu">
+                        <ul className="drawer-menu-inner">
+                            <li><a href="" className="drawer-menu-a"><i class="fas fa-home"></i>ホーム</a></li>
+                            <li><a href="" className="drawer-menu-a"><i class="fas fa-tasks"></i>タスク</a></li>
+                            <li><a href="" className="drawer-menu-a"><i class="far fa-comment-alt"></i>トーク</a></li>
+                            <li><a href="" className="drawer-menu-a"><i class="fas fa-book-open"></i>ノート</a></li>
+                            <li><a href="" className="drawer-menu-a"><i class="far fa-folder"></i>ファイル共有</a></li>
+                            <li><a href="" className="drawer-menu-a"><i class="far fa-file-alt"></i>レポート</a></li>
+                        </ul>                
+                    </div>
+                </div>
+
+                <div className="side">
+                    {/*検索*/}
+                    <div className="search-box">
+                        <input type="text" className="search-text" placeholder="&#xf002;キーワードを入力"></input>
+                    </div>
+                    
+                    <div className="folder">
+                        {/* フォルダ追加
+                        <div className="folder-add"><i class="fas fa-folder-plus"></i>追加</div> */}
+                        {/*フォルダ一覧*/}
+                        <ul className="folder-list">
+                            <li><a href="" className="folder-a"><i class="far fa-folder"></i>テストフォルダ</a><a href=""><i class="fas fa-ellipsis-h"></i></a></li>
+                            <li><a href="" className="folder-a"><i class="far fa-folder"></i>テストフォルダ</a><a href=""><i class="fas fa-ellipsis-h"></i></a></li>
+                            <li><a href="" className="folder-a"><i class="far fa-folder"></i>テストフォルダ</a><a href=""><i class="fas fa-ellipsis-h"></i></a></li>
+                            <li><a href="" className="folder-a"><i class="far fa-folder"></i>テストフォルダ</a><a href=""><i class="fas fa-ellipsis-h"></i></a></li>
+                            <li><a href="" className="folder-a"><i class="far fa-folder"></i>テストフォルダ</a><a href=""><i class="fas fa-ellipsis-h"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="side">
+                    {/*ファイル共有*/}
+                    <div className="side">
+                        <div className="file_sharing">
+                            <h5>ファイル共有</h5>
+                            <h6>フォルダ名</h6>
+                        </div>
+                    </div>
+                    <div className="side">
+                        <i class="fas fa-cloud-download-alt"></i>
+                    </div>
+                </div>
             </div>
         )
     }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,15 +16,101 @@
 
 
     <style>
-        body {
-            font-family: 'Nunito';
-        }
-
-        .search {
-            font-family:"Font Awesome 5 Free";
-            font-weight:800;
-
-        }
+        *{
+                margin: 0px;
+                padding: 0px;
+            }
+            body{
+                color: #62ABB6;
+                font-size: 21px;
+                letter-spacing: 0.05em;
+                margin-left: auto;
+                margin-right: auto;
+                font-family: 'Noto Sans JP', 'sans-serif';
+            }
+            html{
+                
+            }
+            .side{
+                display: inline-block;
+            }
+            /*ドロワーメニュー*/
+            a.drawer-menu-a{
+                text-decoration: none;
+                color:#ffffff;
+            }
+            .drawer-menu{
+                width: 18vw;
+                height:1080px;
+                background: #62ABB6;
+                list-style: none;
+            }
+            ul.drawer-menu-inner li{
+                margin-bottom: 30px;
+                padding-left: 25px;
+            }
+            
+            /*検索*/
+            .search-box{
+                background: #CCE3E3;
+                position: fixed;
+                top: 0px;
+            }
+            .search-text{
+                margin: 30px 70px;
+                width: 24vw;
+                height: 40px;
+                border: none;
+                border-radius:4px;
+                color: #00697A;
+                font-family: "Font Awesome 5 Free", 'Noto Sans JP', 'sans-serif';
+                font-weight: 600;
+            }
+            /*フォルダー追加*/
+            .folder-add{
+                height: 50px;
+                background: #FFFFFF;
+                color: #00697A;
+                position: fixed;
+                top: 100px;
+                line-height: 50px;
+                text-align: center;
+                letter-spacing: 10px;
+                margin: 0 auto;
+            }
+            /*フォルダ一覧*/
+            .folder{
+                width: 24vw;
+                height:981px;
+                /* position: fixed;
+                top: 50px; */
+                position: fixed;
+                top: 100px;   
+                
+            }
+            .folder-list{
+                list-style: none;
+                margin-left: 50px;
+            }
+            a.folder-a{
+                text-decoration: none;
+                color: #00697A;
+            }
+            .fa-folder{
+                color: #00697A;
+                
+            }
+            /*ファイル共有*/
+            .file_sharing{
+                background: #E9F2F3;
+                width: 60vw;
+                position: fixed;
+                top: 100px;
+            }
+            h6{
+                color: #8AA0A0;
+                font-weight: 450;
+            }
     </style>
 </head>
 
