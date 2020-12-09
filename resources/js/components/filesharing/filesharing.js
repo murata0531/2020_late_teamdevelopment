6e9faf8ca80df75955da9ba5c298dbbba26cc95d
murata0531 <coless115@gmail.com>
2020-12-09 13:09:43 +0900

    filesharing

--- resources/js/components/filesharing/filesharing.js
+++ resources/js/components/filesharing/filesharing.js
@@ -2,5 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export default class FileSharing extends React.Component {
-    
+    render(){
+        return (
+            
+        )
+    }
+
+
 }
