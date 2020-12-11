b7070850a4f649f877f0e3285a11778677c3ce33
murata0531 <coless115@gmail.com>
2020-12-11 16:23:35 +0900

    a

---
+++ resources/views/main.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>arael</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" rel="stylesheet">
+    <!-- Styles -->
+
+
+
+
+    <style>
+        html,body {
+            margin:0;
+            padding:0;
+            background-image:url("../images/")
+        }
+
+        
+    </style>
+    
+    <!-- ここにアプリのscriptタグを貼り付けます。 -->
+
+        <!-- The core Firebase JS SDK is always required and must be listed first -->
+        <!-- <script src="https://www.gstatic.com/firebasejs/7.17.1/firebase-app.js"></script> -->
+
+        <!-- TODO: Add SDKs for Firebase products that you want to use
+            https://firebase.google.com/docs/web/setup#available-libraries -->
+        <!-- <script src="https://www.gstatic.com/firebasejs/7.17.1/firebase-analytics.js"></script>
+
+        <script src="https://www.gstatic.com/firebasejs/7.9.3/firebase-auth.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/7.9.3/firebase-database.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/7.6.1/firebase-storage.js"></script> -->
+        <!-- <script> -->
+            <!-- // Your web app's Firebase configuration
+            var firebaseConfig = {
+                
+            };
+            // Initialize Firebase
+            firebase.initializeApp(firebaseConfig);
+            firebase.analytics(); -->
+        </script>
+</head>
+
+<body>
+    
+</body>
+
+</html>
