b36f965096df1d42ca923fb5b75f74063b46b556
murata0531 <coless115@gmail.com>
2020-12-26 09:58:40 +0900

    sw

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,3 +13,15 @@ require('./bootstrap');
  */
 
 require('./components/Example');
+
+window.addEventListener('load', () => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js').
+        then(() => {
+          console.log('ServiceWorker registered')
+        }).
+        catch((error) => {
+          console.warn('ServiceWorker error', error)
+        })
+    }
+  })
