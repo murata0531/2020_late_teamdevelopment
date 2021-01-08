2201d9b4c79d252514920b1921f359bb0f3d6dbb
murata0531 <coless115@gmail.com>
2021-01-08 20:44:06 +0900

    a

--- resources/views/userlogin.blade.php
+++ resources/views/userlogin.blade.php
@@ -33,6 +33,7 @@
                 width:100%;
                 opacity:0.4;
             }
+
             .opacity-box {
                 position:absolute;
                 height:100%;
@@ -41,13 +42,13 @@
                 overflow:hidden;
                 justify-content:center;
                 align-items:center;
-                opacity:0.5;
+                opacity:0.7;
             }
 
             .box {
                 position:relative;
-                height:80vh;
-                width:60vw;
+                height:85vh;
+                width:50vw;
                 display:flex;
                 justify-content:center;
                 background-color:#7C7C7C;
@@ -62,25 +63,118 @@
                 justify-content:center;
                 align-items:center;
                 color:rgba(255,255,255,1);
-                font-size:2em;
+                font-size:2vw;
             }
 
             .login-form {
                 position:relative;
-                height:60%;
+                height:50%;
                 width:100%;
-                background-color:blue;
                 display:flex;
                 flex-direction: column;
+                font-size:1.2vw;
+            }
+
+            .mail, .invalid-mail, .pass, .invalid-pass, .signin {
+                position:relative;
+                width:100%;
+                height:100%;
+                display:flex;
+                flex-direction: row;
+                align-items:center;
+                justify-content:center;
+            }
+
+            .mail-text, .pass-text {
+                position:relative;
+                height:100%;
+                width:45%;
+                display:flex;
+                flex-direction:row;
+                justify-content:center;
+                align-items:center;
+            }
+
+            .mail-address, .password {
+                position:relative;
+                height:100%;
+                width:55%;
+                display:flex;
+                align-items:center;
+            }
+
+            .mail-address > input, .password > input {
+                position:relative;
+                height:60%;
+                width:80%;
+                border-radius:10px;
+                text-align:center;
+                border:none;
+                background-color:rgba(255,255,255,1);
+                color:#00697A;
+                font-size:1.2vw;
+
+            }
+
+            .signin {
+                position:relative;
+                height:100%;
+                width:100%;
+                display:flex;
+                justify-content:center;
+                align-items:center;
+                opacity:1;
+            }
+
+            .signin > input {
+                position:relative;
+                height:100%;
+                width:80%;
+                border-radius:10px;
+                background-color:#00DA9F;
+                cursor:hand;
+                cursor:pointer;
+                opacity:1;
+                color:rgba(255,255,255,1);
+                font-size:1.2vw;
+
             }
 
             .toregister {
                 position:relative;
-                height:20%;
+                height:30%;
                 width:100%;
-                background-color:green;
+                display:flex;
+                justify-content:center;
+                align-items:center;
+                flex-flow:column;
             }
 
+            .toregister-title {
+                position:relative;
+                display:flex;
+                flex-direction:row;
+                width:100%;
+                align-items:center;
+            }
+
+            hr {
+                position:relative;
+                width:20%;
+                background-color:white;
+                border:none;
+                height:1px;
+            }
+
+            p {
+                font-size:1vw;
+            }
+
+            a {
+                font-size:1vw;
+                color:white;
+                text-decolation:none;
+            }
         </style>
         
     </head>
@@ -88,18 +182,19 @@
     <body>
 
         <img src="{{ asset('images/arael-top-back.png') }}" class="arael-top">
+
         <div class="opacity-box">
             <div class="box">
                 <div class="title">ユーザサインイン</div>
                 <form method="post" class="login-form">
                 @csrf
                     <div class="mail">
-                        <div class="mail-text"><i class="far fa-envelope"></i>メールアドレス</div>
+                        <div class="mail-text"><i class="far fa-envelope"></i>　メールアドレス</div>
                         <div class="mail-address"><input type="email" placeholder="example@example.com"></input></div>
                     </div>
                     <div class="invalid-mail"></div>
                     <div class="pass">
-                        <div class="pass-text"><i class="fas fa-lock"></i>パスワード</div>
+                        <div class="pass-text"><i class="fas fa-lock"></i>　パスワード　　</div>
                         <div class="password"><input type="password" placeholder="aaa"></input></div>
                     </div>
                     <div class="invalid-pass"></div>
@@ -107,7 +202,14 @@
                     <div class="signin"><input type="submit" value="サインイン"></input></div>
                 </form>
 
-                <div class="toregister"></div>
+                <div class="toregister">
+                    <div class="toregister-title">
+                        <hr>
+                        <p>ARAELの新しい仲間ですか？</p>
+                        <hr>
+                    </div>
+                    <a href="">アカウントをお持ちでない方はこちらへ</a>
+                </div>
             </div>
         </div>
 
